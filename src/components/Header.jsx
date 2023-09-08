1fc6f0d5c5eac92388d74ccc804b663be131a42e
sooboi <dlgbrkd123@gmail.com>
2023-09-09 00:27:01 +0900

    Edit : header 수정 ( 카테고리 옵션 레이아웃 추가)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,12 @@ export default function Header() {
         <h1 className="ml-4">Soo</h1>
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
+        <select name="category" id="category">
+          <option value="all">전체</option>
+          <option value="top">상의</option>
+          <option value="bottom">하의</option>
+          <option value="shoes">신발</option>
+        </select>
         <div className="border border-gray-600 p-2">
           <input type="text" placeholder="검색어를 입력하세요." />
           <button>
