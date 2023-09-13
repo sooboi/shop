7c4fba32e650144f761b0f7d7a1964ae0bc6a946
sooboi <dlgbrkd123@gmail.com>
2023-09-13 19:33:29 +0900

    Edit : .env 오류 해결 : 오타 문제 발견후 수정했지만 해결되지 않음 -> 새로 작성(해결) 기록

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: process.env.REACT_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
