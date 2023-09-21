2da5157e34513b0b54144ae7565bd5ea36071db3
sooboi <dlgbrkd123@gmail.com>
2023-09-22 00:57:09 +0900

    Backup : Firebase 배포가 망쳐놓은 파일 복구 ... ( 울뻔 )

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Product from "../components/Product";
-import { useNavigate } from "react-router";
 import Footer from "../components/Footer";
 
 export default function Products() {
-  const navigate = new useNavigate();
-
   return (
     <>
       <Product />
