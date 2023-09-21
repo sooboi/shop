8b3f66bed748c3ab19e47f00f700f3dba656d47c
sooboi <dlgbrkd123@gmail.com>
2023-09-22 00:58:43 +0900

    Backup : Firebase 배포가 망쳐놓은 파일 복구 ... ( 울뻔 )

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ text, onClick }) {
   return (
     <button
-      className="bg-brand text-white py-2 px-4 font-semibold rounded-sm hover:brightness-125"
+      className="bg-brand text-white py-2 px-4 font-semibold rounded-sm hover:brightness-110"
       onClick={onClick}
     >
       {text}
